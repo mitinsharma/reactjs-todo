6b5ba9f5d05c760dd22157e8783cfae2ee382fa0
Mitin Sharma <er.mitinsharma@gmail.com>
2020-08-23 15:24:12 -0400

    style added to itemlist

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
       },{
         id: 2,
       title: 'todo 2',
-      completed: false
+      completed: true
       },{
         id: 3,
       title: 'todo 3',
--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -2,10 +2,37 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
+    
+    getStyle = () => {
+        /*
+        if(this.props.todo.completed) {
+            return {
+                textDecoration: 'line-through'
+            }
+        }
+        return {
+            textDecoration: 'none'
+        }
+        */
+
+        return {
+            backgroundColor: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+        }
+    }
+    
+    markComplete = (e) => {
+        console.log(this.props)
+    }
+
+
     render() {
         return (
-            <div>
-                <p>{ this.props.todo.title }</p>
+            <div style={ this.getStyle() }>
+                <input type="checkbox" onChange={this.markComplete}/> { ' ' }
+                { this.props.todo.title }
             </div>
         )
     }
@@ -15,6 +42,10 @@ export class TodoItem extends Component {
 //Proptypes
 TodoItem.propTypes = {
     todo: PropTypes.object.isRequired
+}
+
+const itemStyle = {
+    backgroundColor: '#f4f4f4' 
 }
 
 export default TodoItem
