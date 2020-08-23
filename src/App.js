5cda307389570d4942f3f375c5189b54c266837e
Mitin Sharma <er.mitinsharma@gmail.com>
2020-08-23 14:32:59 -0400

    New Todo component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Todos from './components/Todos';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Todos></Todos>
     </div>
   );
 }
