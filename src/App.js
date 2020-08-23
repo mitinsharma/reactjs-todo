4f620977c8e7f7642aae80ffa55ab951ced0a287
Mitin Sharma <er.mitinsharma@gmail.com>
2020-08-23 14:53:28 -0400

    passing state to todo component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,32 @@ import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component {
-  
+  state = {
+    todos: [
+      {
+        id: 1,
+      title: 'take out the trash',
+      completed: false
+      },{
+        id: 2,
+      title: 'todo 2',
+      completed: false
+      },{
+        id: 3,
+      title: 'todo 3',
+      completed: false
+      },{
+        id: 4,
+      title: 'todo 4',
+      completed: false
+      }
+      
+    ]
+  }
   render() {
     return (
       <div className="App">
-        <Todos/>
+        <Todos todos={this.state.todos}/>
       </div>
     );
   }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function Todos() {
-  return (
-    <div>
-      <h1>Todos</h1>
-    </div>
-  );
+class Todos extends Component {
+    render(){
+        console.log(this.props.todos);
+        return (
+            <div className="Todos">
+            <h1>Todos</h1>
+            </div>
+        );
+    }
 }
 
 export default Todos;
