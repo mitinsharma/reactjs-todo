28f15ba4daddbb61a49e068fa7dfe2a440c2e6ac
Mitin Sharma <er.mitinsharma@gmail.com>
2020-08-23 15:06:35 -0400

    Added proptype isRequireed

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-export class Todoitem extends Component {
+export class TodoItem extends Component {
     render() {
         return (
             <div>
@@ -10,4 +11,10 @@ export class Todoitem extends Component {
     }
 }
 
-export default Todoitem
+
+//Proptypes
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired
+}
+
+export default TodoItem
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react';
-import Todoitem from './Todoitem';
+import TodoItem from './Todoitem';
+import PropTypes from 'prop-types';
 
 class Todos extends Component {
     render(){
         //console.log(this.props.todos);
         return this.props.todos.map((todo) => (
-            <Todoitem todo={todo}/>
+            <TodoItem key={todo.id} todo={todo}/>
         ));
     }
+}
+
+//Proptypes
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired
 }
 
 export default Todos;
