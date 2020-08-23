842ef3485b46c2508fc8d3d0426aaf41d3665959
Mitin Sharma <er.mitinsharma@gmail.com>
2020-08-23 14:40:10 -0400

    Convert functional component to class component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Todos from './components/Todos';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Todos></Todos>
-    </div>
-  );
+class App extends Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <Todos/>
+      </div>
+    );
+  }
+
 }
 
 export default App;
