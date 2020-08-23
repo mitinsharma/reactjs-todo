52c2420e82d7f6c7cc85e38d7647c6ab51f7869e
Mitin Sharma <er.mitinsharma@gmail.com>
2020-08-23 16:49:57 -0400

    router added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Todos from './components/Todos';
 import Addtodo from './components/Addtodo';
 import Header from './components/layout/Header';
+import About from './components/pages/About';
 import {v4 as uuidv4} from 'uuid';
 
 import './App.css';
@@ -55,15 +57,22 @@ class App extends Component {
 
   render() {
     return (
+      <Router>
       <div className="App">
         <div className="container">
           <Header/>
-          <Addtodo addTodo={this.addTodo}/>
-          <Todos todos={this.state.todos} 
-          markComplete={this.markComplete}
-          delTodo = {this.delTodo}/>
+          <Route exact path="/" render={props => (
+            <React.Fragment>
+              <Addtodo addTodo={this.addTodo}/>
+              <Todos todos={this.state.todos} 
+              markComplete={this.markComplete}
+              delTodo = {this.delTodo}/>
+            </React.Fragment>
+          )}/>
+          <Route path="/about" component = {About} />
         </div>
       </div>
+      </Router>
     );
   }
 
